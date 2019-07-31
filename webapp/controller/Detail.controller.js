ef2acc68c1c3231a22dc9b88e8cb395a798080f8
phani <phani.kumar.vankadari@gmail.com>
2019-07-31 10:01:53 +0000

    added detail page and navigation done

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -3,31 +3,26 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("com.local.file.to.screen.File2Screen.controller.Main", {
+	return Controller.extend("com.local.file.to.screen.File2Screen.controller.Detail", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.local.file.to.screen.File2Screen.view.Main
+		 * @memberOf com.local.file.to.screen.File2Screen.view.Detail
 		 */
 		onInit: function () {
 
 		},
 		
-go2Details:function(e){
-		
-		var context = e.getSource().getBindingContext();
-		
-		var oComp= this.getOwnerComponent()	;
-		var oRouter = oComp.getRouter();
-		oRouter.navTo("detail",{path23:context.getPath().replace("/products/","")});
-
-
-}
+		goBack:function(){
+			
+			window.history.back();
+			
+		}
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.local.file.to.screen.File2Screen.view.Main
+		 * @memberOf com.local.file.to.screen.File2Screen.view.Detail
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -36,7 +31,7 @@ go2Details:function(e){
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.local.file.to.screen.File2Screen.view.Main
+		 * @memberOf com.local.file.to.screen.File2Screen.view.Detail
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -44,7 +39,7 @@ go2Details:function(e){
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.local.file.to.screen.File2Screen.view.Main
+		 * @memberOf com.local.file.to.screen.File2Screen.view.Detail
 		 */
 		//	onExit: function() {
 		//
